a3e04faacc506e41fe972655884ba9ddd564807e
mwaddtul <115225198+mwaddtul@users.noreply.github.com>
2023-01-24 11:05:34 +0700

    ganti

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'To-do list application',
   tagline: 'From groceries to goals, we have got you covered.',
-  url: 'https://docs-indol.vercel.app/',
+  url: 'https://homenutrition.vercel.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
