839a1bce46f6ddc6accf18e12de26298682696d7
mwaddtul <115225198+mwaddtul@users.noreply.github.com>
2023-01-25 18:24:47 +0700

    ganti

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'HomeNutrition',
+  title: 'HomeNutrition ',
   tagline: 'From groceries to goals, we have got you covered.',
   url: 'https://homenutrition-uas-trpl.vercel.app/',
   baseUrl: '/',
@@ -57,7 +57,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'To-do List Application',
+        title: 'HomeNutrition',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
